df9aa8fc1483e73d12b3ba362f9e4ebcebfc48fd
Suzie Brown <suzie.brown42@gmail.com>
2025-02-08 14:27:55 +0000

    Add basic routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,6 @@ export default async function Home() {
   void api.post.getLatest.prefetch();
 
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col bg-gradient-to-b from-orange-300 to-orange-500 text-black">
-        <div className="container flex flex-col justify-start gap-4 p-4">
-          <h1 className="text-2xl font-bold tracking-tight">Browse puzzles</h1>
-
-          <div className="flex w-full justify-center flex-wrap">
-            {puzzles.map((p) => (
-              <PuzzlePreview puzzle={p} key={p.id} />
-            ))}
-          </div>
-        </div>
-      </main>
-    </HydrateClient>
+          <h1 className="text-2xl font-bold tracking-tight">Welcome</h1>
   );
 }
