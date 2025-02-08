fe221aa4ea2f475c0e998b5901fa7d77fc406f08
Suzie Brown <suzie.brown42@gmail.com>
2025-02-08 13:22:40 +0000

    Initialise db schema and seed basic data

--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -1,21 +1,21 @@
-import LogoBlock from "./logo-block"
+import LogoBlock from "./logo-block";
 
 export default function Header() {
-    return (
-        <div className="bg-black p-2 text-white h-xl flex justify-items-start">
-            <div className="grid grid-cols-3 grid-rows-3 gap-1">
-                <LogoBlock letter="T" />
-                <LogoBlock letter="A" />
-                <LogoBlock letter="N" />
-                <LogoBlock letter="G" />
-                <LogoBlock letter="E" />
-                <LogoBlock letter="R" />
-                <LogoBlock letter="I" />
-                <LogoBlock letter="N" />
-                <LogoBlock letter="E" />
-            </div>
+  return (
+    <div className="h-xl flex justify-items-start bg-black p-2 text-white">
+      <div className="grid grid-cols-3 grid-rows-3 gap-1">
+        <LogoBlock letter="T" />
+        <LogoBlock letter="A" />
+        <LogoBlock letter="N" />
+        <LogoBlock letter="G" />
+        <LogoBlock letter="E" />
+        <LogoBlock letter="R" />
+        <LogoBlock letter="I" />
+        <LogoBlock letter="N" />
+        <LogoBlock letter="E" />
+      </div>
 
-            <i className="ml-4 mt-auto mb-1 text-orange-500">Set by Tangerine</i>
-        </div>
-    );
+      <i className="mb-1 ml-4 mt-auto text-orange-500">Set by Tangerine</i>
+    </div>
+  );
 }
