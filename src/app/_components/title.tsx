6da91eddf8a325324ccc3d1089293b5aa48d8b64
Suzie Brown <suzie.brown42@gmail.com>
2025-02-16 12:28:01 +0000

    Fix Insiders data to match PDF version

--- src/app/_components/title.tsx
+++ src/app/_components/title.tsx
@@ -1,3 +1,5 @@
 export default function Title(props: { children: React.ReactNode }) {
-  return <h1 className="flex-grow text-2xl font-semibold h-fit">{props.children}</h1>;
+  return (
+    <h1 className="h-fit flex-grow text-2xl font-semibold">{props.children}</h1>
+  );
 }
