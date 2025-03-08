2ba1c8f2e82b4598923b50412389caf74b021465
Suzie Brown <suzie.brown42@gmail.com>
2025-03-08 18:20:43 +0000

    Display crossword grid; improve layout

--- src/app/puzzle/[id]/page.tsx
+++ src/app/puzzle/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Crossword from "~/app/_components/crossword";
+import DownloadButton from "~/app/_components/download-button";
 import { api } from "~/trpc/server";
 import Title from "../../_components/title";
-import DownloadButton from "~/app/_components/download-button";
 
 export default async function ViewPuzzle({
   params,
@@ -25,6 +25,16 @@ export default async function ViewPuzzle({
         <p className="text-pretty rounded-lg border border-white/30 bg-white/20 px-4 py-2">
           {puzzleDetails.blurb}
         </p>
+      )}
+
+      {puzzleDetails?.imageUrl && (
+        <img
+          src={
+            process.env.NEXT_PUBLIC_IMAGES_BLOB_PREFIX + puzzleDetails.imageUrl
+          }
+          alt={puzzleDetails.imageUrl}
+          className="max-w-[600px]"
+        />
       )}
 
       {puzzleDetails?.crossword && (
