94c63452ef4ffbebea86c534d477a845d5b587aa
Suzie Brown <32639147+suziebrown@users.noreply.github.com>
2025-05-18 17:12:04 +0100

    Crossword helpers: add more tests (#18)
    * Add more test cases for mapMyCrosswordData
    
    * Add some tests for lower-level functions
    
    * Add helper to calculate human number
    
    * Update mycrossword package

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "npm run start",
+    command: "npm run dev",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
