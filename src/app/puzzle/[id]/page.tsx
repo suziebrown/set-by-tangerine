6227337bd6e86f2b56d40a0ef10099a8570cb802
Suzie Brown <suzie.brown42@gmail.com>
2025-06-20 20:42:15 +0100

    Add puzzle title to page title

--- src/app/puzzle/[id]/page.tsx
+++ src/app/puzzle/[id]/page.tsx
@@ -1,5 +1,19 @@
 import { api } from "~/trpc/server";
 import PuzzleDetail from "./puzzle-detail";
+import { type Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+  const id = (await params).id;
+  const puzzleDetails = await api.puzzle.getById({ id: parseInt(id) });
+
+  return {
+    title: puzzleDetails.title + " | Set by Tangerine",
+  };
+}
 
 export default async function ViewPuzzle({
   params,
