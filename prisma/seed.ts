9424c5da31bef8352d8d73763503c3ec2c385047
Suzie Brown <suzie.brown42@gmail.com>
2025-06-20 19:25:12 +0100

    Format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -125,7 +125,11 @@ async function seedData(): Promise<void> {
       downloadUrl: "paws.pdf",
       imageUrl: "insiders_grid.jpg",
       tags: {
-        connect: [{ label: "crossword" }, { label: "cryptic" }, { label: "jumbo" }],
+        connect: [
+          { label: "crossword" },
+          { label: "cryptic" },
+          { label: "jumbo" },
+        ],
       },
       crossword: {
         create: {
