77097a9091327f3a347947be3301239ce405ec79
Suzie Brown <32639147+suziebrown@users.noreply.github.com>
2025-06-21 12:15:26 +0100

    Make header logo a nav link (#28)

--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import LogoBlock from "./logo-block";
 
 export default function Header() {
   return (
     <div className="h-xl flex justify-items-start bg-black p-2 text-white">
-      <div className="grid grid-cols-3 grid-rows-3 gap-1">
+      <Link href="/" className="grid grid-cols-3 grid-rows-3 gap-1">
         <LogoBlock letter="T" />
         <LogoBlock letter="A" />
         <LogoBlock letter="N" />
@@ -13,9 +14,11 @@ export default function Header() {
         <LogoBlock letter="I" />
         <LogoBlock letter="N" />
         <LogoBlock letter="E" />
-      </div>
+      </Link>
 
-      <i className="mb-1 ml-4 mt-auto text-orange-500">Set by Tangerine</i>
+      <Link href="/" className="mb-1 ml-4 mt-auto italic text-orange-500">
+        Set by Tangerine
+      </Link>
     </div>
   );
 }
