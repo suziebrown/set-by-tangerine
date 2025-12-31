d28301b6e0d104981ef892f7514f1ad092c7b285
Suzie Brown <suzie.brown42@gmail.com>
2025-12-31 15:03:56 +0000

    Move browse page to client-side rendering and handle loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,42 @@
+"use client";
+
 import { PuzzlePreview } from "@app/browse/puzzle-preview";
-import { api } from "~/trpc/server";
+import { api } from "~/trpc/react";
 import Title from "@components/title";
 import { Badge } from "./_components/badge";
 import { type Tag } from "@prisma/client";
+import { Loader } from "./_components/loader";
 
-export default async function Browse() {
-  const puzzles = await api.puzzle.list();
-  const tags = await api.tag.list();
+export default function Browse() {
+  const puzzlesQuery = api.puzzle.list.useQuery();
+  const tagsQuery = api.tag.list.useQuery();
 
-  return (
-    <>
-      <Title>Browse puzzles</Title>
+  if (puzzlesQuery.isLoading || tagsQuery.isLoading) return <Loader />;
 
-      <div>
-        Filter by tags:
-        <ul className="flex flex-wrap gap-1">
-          {tags.map((t: Tag) => (
-            <li key={t.id}>
-              <Badge label={t.label} />
-            </li>
+  if (puzzlesQuery.isSuccess && tagsQuery.isSuccess) {
+    return (
+      <>
+        <Title>Browse puzzles</Title>
+
+        <div>
+          Filter by tags:
+          <ul className="flex flex-wrap gap-1">
+            {tagsQuery.data.map((t: Tag) => (
+              <li key={t.id}>
+                <button type="button">
+                  <Badge label={t.label} />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex w-full flex-wrap justify-start gap-4">
+          {puzzlesQuery.data.map((p) => (
+            <PuzzlePreview puzzle={p} key={p.id} />
           ))}
-        </ul>
-      </div>
-
-      <div className="flex w-full flex-wrap justify-start gap-4">
-        {puzzles.map((p) => (
-          <PuzzlePreview puzzle={p} key={p.id} />
-        ))}
-      </div>
-    </>
-  );
+        </div>
+      </>
+    );
+  }
 }
