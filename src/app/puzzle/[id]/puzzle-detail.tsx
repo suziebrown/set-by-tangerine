92c180ca54748cf72fafe3ea6407e678f6c9af35
Suzie Brown <suzie.brown42@gmail.com>
2025-12-31 13:26:22 +0000

    Fix lint errors

--- src/app/puzzle/[id]/puzzle-detail.tsx
+++ src/app/puzzle/[id]/puzzle-detail.tsx
@@ -17,42 +17,46 @@ export default function PuzzleDetail({ id }: { id: string }) {
     data: puzzleDetails,
     isLoading,
     isError,
+    isSuccess,
   } = api.puzzle.getById.useQuery({ id });
 
   if (isLoading) return <p>Loading...</p>;
+
   if (isError) return <p>Uh-oh!</p>;
 
-  const crosswordData = parseCrosswordDataJson(puzzleDetails.crossword?.data);
+  if (isSuccess) {
+    const crosswordData = parseCrosswordDataJson(puzzleDetails.crossword?.data);
 
-  return (
-    <>
-      <div className="space-between flex w-full items-center">
-        <span className="inline-flex flex-grow items-center">
-          <Title>{puzzleDetails.title}</Title>
+    return (
+      <>
+        <div className="space-between flex w-full items-center">
+          <span className="inline-flex flex-grow items-center">
+            <Title>{puzzleDetails.title}</Title>
 
-          <button
-            onClick={() => setShowInfo((show) => !show)}
-            className="ml-2 h-5 w-5 rounded-full border border-orange-700 bg-orange-500 hover:bg-orange-600"
-            aria-label={showInfo ? "Hide info" : "Show info"}
-          >
-            <FontAwesomeIcon className="mb-1 h-3 w-3" icon={faInfo} />
-          </button>
-        </span>
+            <button
+              onClick={() => setShowInfo((show) => !show)}
+              className="ml-2 h-5 w-5 rounded-full border border-orange-700 bg-orange-500 hover:bg-orange-600"
+              aria-label={showInfo ? "Hide info" : "Show info"}
+            >
+              <FontAwesomeIcon className="mb-1 h-3 w-3" icon={faInfo} />
+            </button>
+          </span>
 
-        {puzzleDetails.downloadUrl && (
-          <DownloadButton url={puzzleDetails.downloadUrl} />
+          {puzzleDetails.downloadUrl && (
+            <DownloadButton url={puzzleDetails.downloadUrl} />
+          )}
+        </div>
+
+        {showInfo && <InfoBox puzzleDetails={puzzleDetails} />}
+
+        {puzzleDetails.crossword && crosswordData && (
+          <Crossword
+            id={puzzleDetails.crossword.id}
+            instructions={puzzleDetails.crossword.instructions}
+            data={crosswordData}
+          />
         )}
-      </div>
-
-      {showInfo && <InfoBox puzzleDetails={puzzleDetails} />}
-
-      {puzzleDetails.crossword && crosswordData && (
-        <Crossword
-          id={puzzleDetails.crossword.id}
-          instructions={puzzleDetails.crossword.instructions}
-          data={crosswordData}
-        />
-      )}
-    </>
-  );
+      </>
+    );
+  }
 }
