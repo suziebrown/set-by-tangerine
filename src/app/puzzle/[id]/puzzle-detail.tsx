719dea25fe87062309d7f51d9e4a15bb1c3fc8b9
Suzie Brown <32639147+suziebrown@users.noreply.github.com>
2025-12-31 14:15:10 +0000

    Show loader while puzzle detail is loading (#44)
    * Handle loading and error states on puzzle detail page
    * Add loader component

--- src/app/puzzle/[id]/puzzle-detail.tsx
+++ src/app/puzzle/[id]/puzzle-detail.tsx
@@ -3,51 +3,61 @@
 import DownloadButton from "@components/download-button";
 import { faInfo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { type PuzzleWithCrossword } from "prisma/types";
 import { useState } from "react";
 import Crossword from "~/app/puzzle/[id]/crossword/crossword";
 import Title from "../../_components/title";
 import InfoBox from "./info-box";
 import { parseCrosswordDataJson } from "~/helpers/crossword-helpers";
+import { api } from "~/trpc/react";
+import { Loader } from "~/app/_components/loader";
 
-export default function PuzzleDetail({
-  puzzleDetails,
-}: {
-  puzzleDetails: PuzzleWithCrossword;
-}) {
+export default function PuzzleDetail({ id }: { id: string }) {
   const [showInfo, setShowInfo] = useState(false);
 
-  const crosswordData = parseCrosswordDataJson(puzzleDetails?.crossword?.data);
+  const {
+    data: puzzleDetails,
+    isLoading,
+    isError,
+    isSuccess,
+  } = api.puzzle.getById.useQuery({ id });
 
-  return (
-    <>
-      <div className="space-between flex w-full items-center">
-        <span className="inline-flex flex-grow items-center">
-          <Title>{puzzleDetails.title}</Title>
+  if (isLoading) return <Loader />;
 
-          <button
-            onClick={() => setShowInfo((show) => !show)}
-            className="ml-2 h-5 w-5 rounded-full border border-orange-700 bg-orange-500 hover:bg-orange-600"
-            aria-label={showInfo ? "Hide info" : "Show info"}
-          >
-            <FontAwesomeIcon className="mb-1 h-3 w-3" icon={faInfo} />
-          </button>
-        </span>
+  if (isError) return <p>Uh-oh!</p>;
 
-        {puzzleDetails.downloadUrl && (
-          <DownloadButton url={puzzleDetails.downloadUrl} />
+  if (isSuccess) {
+    const crosswordData = parseCrosswordDataJson(puzzleDetails.crossword?.data);
+
+    return (
+      <>
+        <div className="space-between flex w-full items-center">
+          <span className="inline-flex flex-grow items-center">
+            <Title>{puzzleDetails.title}</Title>
+
+            <button
+              onClick={() => setShowInfo((show) => !show)}
+              className="ml-2 h-5 w-5 rounded-full border border-orange-700 bg-orange-500 hover:bg-orange-600"
+              aria-label={showInfo ? "Hide info" : "Show info"}
+            >
+              <FontAwesomeIcon className="mb-1 h-3 w-3" icon={faInfo} />
+            </button>
+          </span>
+
+          {puzzleDetails.downloadUrl && (
+            <DownloadButton url={puzzleDetails.downloadUrl} />
+          )}
+        </div>
+
+        {showInfo && <InfoBox puzzleDetails={puzzleDetails} />}
+
+        {puzzleDetails.crossword && crosswordData && (
+          <Crossword
+            id={puzzleDetails.crossword.id}
+            instructions={puzzleDetails.crossword.instructions}
+            data={crosswordData}
+          />
         )}
-      </div>
-
-      {showInfo && <InfoBox puzzleDetails={puzzleDetails} />}
-
-      {puzzleDetails?.crossword && crosswordData && (
-        <Crossword
-          id={puzzleDetails.crossword.id}
-          instructions={puzzleDetails.crossword.instructions}
-          data={crosswordData}
-        />
-      )}
-    </>
-  );
+      </>
+    );
+  }
 }
