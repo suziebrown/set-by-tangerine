cb1928c8aa7859f0783af7020e71a33993de8d8c
Suzie Brown <suzie.brown42@gmail.com>
2025-12-31 16:02:53 +0000

    Add cross on selected badges

--- src/app/_components/badge.tsx
+++ src/app/_components/badge.tsx
@@ -1,15 +1,20 @@
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import clsx from "clsx";
 
 export function Badge(props: { label: string; selected?: boolean }) {
   return (
     <span
       className={clsx(
-        "inline-block rounded-full bg-orange-500 px-2 py-1 text-xs text-black",
-        { "opacity-80": props.selected === false },
+        "inline-flex rounded-full bg-orange-500 px-2 py-1 align-baseline text-xs text-black",
+        { "opacity-70": props.selected === false },
         { "font-bold": props.selected === true },
       )}
     >
       {props.label}
+      {props.selected && (
+        <FontAwesomeIcon className="ml-1 h-4 w-4" icon={faClose} />
+      )}
     </span>
   );
 }
