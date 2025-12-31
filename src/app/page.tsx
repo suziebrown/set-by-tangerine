81e37f14daad3cb9f8679006965e01f0bde301a2
Suzie Brown <suzie.brown42@gmail.com>
2025-12-31 14:53:47 +0000

    DIsplay all tags at top of browse page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 import { PuzzlePreview } from "@app/browse/puzzle-preview";
 import { api } from "~/trpc/server";
 import Title from "@components/title";
+import { Badge } from "./_components/badge";
+import { type Tag } from "@prisma/client";
 
 export default async function Browse() {
   const puzzles = await api.puzzle.list();
+  const tags = await api.tag.list();
 
   return (
     <>
       <Title>Browse puzzles</Title>
+
+      <div>
+        Filter by tags:
+        <ul className="flex flex-wrap gap-1">
+          {tags.map((t: Tag) => (
+            <li key={t.id}>
+              <Badge label={t.label} />
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div className="flex w-full flex-wrap justify-start gap-4">
         {puzzles.map((p) => (
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { puzzleRouter } from "~/server/api/routers/puzzle";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { tagRouter } from "./routers/tag";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   puzzle: puzzleRouter,
+  tag: tagRouter,
 });
 
 // export type definition of API
